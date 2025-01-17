de113c08694384129af63bf87b3f2964ae2b1755
biao.zhu <biao.zhu@dbappsecurity.com.cn>
2025-01-17 09:10:49 +0800

    feat: 🎸 add 3-misusing-init-functions

---
+++ 3-misusing-init-functions/go-program-initialization-order/go.mod
@@ -0,0 +1,7 @@
+module demo
+
+go 1.23.4
+
+require github.com/samber/lo v1.47.0
+
+require golang.org/x/text v0.21.0 // indirect
---
+++ 3-misusing-init-functions/go-program-initialization-order/pkg3/pkg3.go
@@ -0,0 +1,47 @@
+package pkg3
+
+import (
+	"fmt"
+
+	_ "demo/pkg4"
+)
+
+const (
+	c1  = "c1"
+	c2  = "c2"
+	pkg = "pkg3"
+)
+
+var (
+	_  = constInitCheck(c1)
+	_  = constInitCheck(c2)
+	v1 = variableInit("v1")
+	v2 = variableInit("v2")
+)
+
+func init() {
+	fmt.Printf("%s: first-init  func invoked\n", pkg)
+}
+
+func init() {
+	fmt.Printf("%s: second-init func invoked\n", pkg)
+}
+
+func main() {
+	fmt.Printf("%s: main        func invoked\n", pkg)
+}
+
+func variableInit(name string) string {
+	fmt.Printf("%s: variable %s has been initialized\n", pkg, name)
+	return name
+}
+
+func constInitCheck(name string) string {
+	if name != "" {
+		fmt.Printf("%s: const %s has been initialized\n", pkg, name)
+		return name
+	}
+
+	fmt.Printf("%s: const %s has not been initialized\n", pkg, name)
+	return name
+}
